d3b7b2bfabf27d4b58abd178e8cca4441ecec687
Ahmed Fekry <32654039+AhmedUFekry@users.noreply.github.com>
2023-09-09 05:25:32 +0300

    creating project
    file structure (MVVM) + adding necessary third party libraries (Moya + AlamoFire)

---
+++ PhotosApp/PhotosApp/Modules/AlbumsModule/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Ahmed Fekry on 09/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
